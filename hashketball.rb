16fd82f5e164ec06514d06e8776993c9a7457e17
Dmitry Sychev <axiumone@gmail.com>
2020-06-10 19:07:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -153,7 +153,7 @@ end
 def num_points_scored(player)
   player_helper.each do |hash|
     if hash[:player_name] == player
-      p hash[:points]
+      hash[:points]
       # binding.pry
     end
   end
